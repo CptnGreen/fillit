c16887fbea8c250e30f2a541d1781eba86083f56
Sauce Lisandra <slisandr@il-h4.21-school.ru>
2020-01-28 03:58:58 +0300

    some more norminetting

--- src/update_mlst_status.c
+++ src/update_mlst_status.c
@@ -6,14 +6,32 @@
 /*   By: slisandr <slisandr@student.21-sch...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/28 01:53:43 by slisandr          #+#    #+#             */
-/*   Updated: 2020/01/28 02:00:10 by slisandr         ###   ########.fr       */
+/*   Updated: 2020/01/28 03:57:57 by slisandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
 void	handle_upper_letter(\
-	int *n_of, t_node *cur, char *c_prev_pres, char letter);
+	int *n_of, t_node *cur, char *c_prev_pres, char letter)
+{
+	n_of[OPTS] += 1;
+	if (cur->r->c > ft_toupper(*c_prev_pres) && cur->r->c > letter)
+	{
+		n_of[TETR_PRES] += 1;
+		*c_prev_pres = ft_toupper(cur->r->c);
+	}
+}
+
+void	reset_vars(int *n_of, char *c_prev_pres, char *c_prev_all)
+{
+	n_of[OPTS] = 0;
+	n_of[TETR_ALL] = 0;
+	n_of[TETR_PRES] = 0;
+	n_of[CHOS] = 0;
+	*c_prev_pres = 'A' - 1;
+	*c_prev_all = 'A' - 1;
+}
 
 void	update_mlst_status(t_node **m_lst, int *n_of)
 {
@@ -22,8 +40,7 @@ void	update_mlst_status(t_node **m_lst, int *n_of)
 	t_node	*cur;
 	char	letter;
 
-	n_of[OPTS] = n_of[TETR_ALL] = n_of[TETR_PRES] = n_of[CHOS] = 0;
-	c_prev_pres = c_prev_all = 'A' - 1;
+	reset_vars(n_of, &c_prev_pres, &c_prev_all);
 	cur = (m_lst[0]->role == 'z') ? (m_lst[0]->d) : (m_lst[0]);
 	while (cur->c != '$')
 	{
@@ -40,16 +57,5 @@ void	update_mlst_status(t_node **m_lst, int *n_of)
 			c_prev_all = ft_toupper(cur->r->c);
 		}
 		cur = cur->d;
-	}
-}
-
-void	handle_upper_letter(\
-	int *n_of, t_node *cur, char *c_prev_pres, char letter)
-{
-	n_of[OPTS] += 1;
-	if (cur->r->c > ft_toupper(*c_prev_pres) && cur->r->c > letter)
-	{
-		n_of[TETR_PRES] += 1;
-		*c_prev_pres = ft_toupper(cur->r->c);
 	}
 }
